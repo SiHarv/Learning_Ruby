a793fda5c6086b2be0fb50ff2c5fb10581a49177
siHarv <harveycasane1@gmail.com>
2026-02-02 17:10:33 +0800

    caesar_cipher project

---
+++ caesar_cipher.rb
@@ -0,0 +1,20 @@
+class CaesarCipher
+    def process(str, int){
+
+
+    }
+
+end
+
+start = caesar_cipher
+
+
+print "=== This is Caesar Cipher Project ==="
+
+print "Input a string: \n"
+str = gets.to_s
+
+print "Input int: \n"
+int =gets.to_i
+
+start.CaesarCipher(str, int)
